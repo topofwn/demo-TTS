5c0daba134e60f2d0184ba48ecd6bcfd1a62bb7f
tatran <nguyenta.tran@ogilvy.com>
2018-10-26 16:40:36 +0700

    init demo

---
+++ app/src/main/java/com/luan/demotts/MainActivity.java
@@ -0,0 +1,50 @@
+package com.luan.demotts;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private TextToSpeech t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button start = findViewById(R.id.btnStart);
+        TextView text = findViewById(R.id.txtSpeak);
+        final String toSpeak = text.getText().toString();
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t1.setSpeechRate(0.75f);
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
+
+            }
+        });
+
+    }
+
+    public void onPause() {
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+}
